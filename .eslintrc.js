a0f90b6e85954608c3ba6f2101e55ae01faea9fa
SuperCuteXiaoSi <1531733886@qq.com>
2021-06-19 01:17:09 +0800

    添加prettier格式化工具

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
-  extends: ['standard', 'plugin:vue/essential'],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: ['vue'],
-  rules: {
-    'no-unused-vars': 'off',
-    'vue/no-multiple-template-root': 'off',
-    camelcase: 0
-  }
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	extends: ['standard', 'plugin:vue/essential', 'plugin:prettier/recommended'],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['vue'],
+	rules: {
+		'prettier/prettier': 'error',
+		'no-unused-vars': 'off',
+		'vue/no-multiple-template-root': 'off',
+		camelcase: 0,
+	},
 }
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-<router-view/>
+	<router-view />
 </template>
 
 <script setup>
-import HelloWorld from     './components/HelloWorld.vue'
+import HelloWorld from'./components/HelloWorld.vue'
 
-let   admin='asdasdasdas'
+const  admin='asdasdasdas'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
