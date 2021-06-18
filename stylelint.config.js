84ccc87165871c89e0fbda8b73507e2c2efa03ce
SuperCuteXiaoSi <1531733886@qq.com>
2021-06-18 23:37:06 +0800

    添加eslint

---
+++ stylelint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: ['stylelint-scss', 'stylelint-order'],
+
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  rules: {
+    indentation: 2, // 缩进2个空格
+    'at-rule-no-unknown': [true, {
+      ignoreAtRules: [
+        'mixin', 'extend', 'content', 'include'
+      ]
+    }],
+    'no-descending-specificity': null, // 禁止特异性较低的选择器在特异性较高的选择器之后重写
+    'selector-pseudo-class-no-unknown': [true, {
+      ignorePseudoClasses: ['host']
+    }],
+    // https://github.com/kristerkari/stylelint-scss scsslint规范配置
+    'scss/comment-no-loud': true, // 禁止使用/*注释
+    'scss/comment-no-empty': true // 禁止空注释
+  }
+  // ignoreFiles: ['src/**/styles/variables-*.scss']
+
+}
