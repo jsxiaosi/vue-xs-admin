6915d16e61fbbb321edfb09e1a9ce10605f241f6
SuperCuteXiaoSi <1531733886@qq.com>
2021-06-19 00:27:50 +0800

    测试

--- build/vite/plugin/index.js
+++ build/vite/plugin/index.js
@@ -3,27 +3,24 @@ import vue from '@vitejs/plugin-vue'
 // 按需加载样式配置
 import { configStylePlugin } from './style'
 // 主题切换配置
-import { configThemePlugin } from './theme';
+import { configThemePlugin } from './theme'
 // svg配置
-import { configSvgPlugin } from './svg';
+import { configSvgPlugin } from './svg'
 // 压缩
 import { configCompressPlugin } from './compress'
 
-import viteESLint from '@ehutch79/vite-eslint'
+// import viteESLint from '@ehutch79/vite-eslint'
 
-
-export function createVitePlugins() {
-  const vitePlugins = [
-    vue()
-  ]
+export function createVitePlugins () {
+  const vitePlugins = [vue()]
   vitePlugins.push(configStylePlugin())
 
   vitePlugins.push(configThemePlugin())
 
   vitePlugins.push(configSvgPlugin())
 
-  vitePlugins.push(configCompressPlugin('gzip',true))
+  vitePlugins.push(configCompressPlugin('gzip', true))
 
-  vitePlugins.push(viteESLint())
+  // vitePlugins.push(viteESLint())
   return vitePlugins
 }
