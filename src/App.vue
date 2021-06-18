42308120178ca8c573c9d1e68bfcbb7b02524565
SuperCuteXiaoSi <1531733886@qq.com>
2021-06-19 00:48:26 +0800

    可以吗？

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <script setup>
 import HelloWorld from     './components/HelloWorld.vue'
 
-let admin='asdasdasdas'
+let   admin='asdasdasdas'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
