805e472ffc7c186fe5a65f4ae56d1aea6c3f09c9
SuperCuteXiaoSi <1531733886@qq.com>
2021-10-24 16:45:38 +0800

    feature: 添加构建命令

--- build/vite/build.ts
+++ build/vite/build.ts
@@ -9,11 +9,11 @@ export function createViteBuild(): BuildOptions {
 		// 启用/禁用 CSS 代码拆分。当启用时，在异步 chunk 中导入的 CSS 将内联到异步 chunk 本身，并在块加载时插入 如果禁用，整个项目中的所有 CSS 将被提取到一个 CSS 文件中。
 		cssCodeSplit: false,
 		// 构建后是否生成 source map 文件。
-		sourcemap: true,
+		sourcemap: false,
 		// 启用/禁用 brotli 压缩大小报告。压缩大型输出文件可能会很慢，因此禁用该功能可能会提高大型项目的构建性能。
 		brotliSize: true,
 		// chunk 大小警告的限制（以 kbs 为单位）
-		chunkSizeWarningLimit: 500,
+		chunkSizeWarningLimit: 2000,
 	}
 	return viteBuild
 }
--- build/vite/resolve.ts
+++ build/vite/resolve.ts
@@ -7,6 +7,11 @@ export function createViteResolve(
 	mode: string,
 	myDirname: string
 ): myResolveOptions {
+	// 解决警告You are running the esm-bundler build of vue-i18n. It is recommended to configure your bundler to explicitly replace feature flag globals with boolean literals to get proper tree-shaking in the final bundle.
+	const I18nAlias: AliasOptions =
+		mode === 'development'
+			? { 'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js' }
+			: {}
 	const viteResolve: myResolveOptions = {
 		// 引用别名配置
 		alias: {
@@ -14,11 +19,11 @@ export function createViteResolve(
 			'@': `${path.resolve(myDirname, 'src')}`,
 			// 配置#别名
 			'#': `${path.resolve(myDirname, 'types')}`,
-			// 解决警告You are running the esm-bundler build of vue-i18n. It is recommended to configure your bundler to explicitly replace feature flag globals with boolean literals to get proper tree-shaking in the final bundle.
-			'vue-i18n': mode === 'development' ? 'vue-i18n/dist/vue-i18n.cjs.js' : '',
+			...I18nAlias,
 		},
 		// 导入时想要省略的扩展名列表。注意，不 建议忽略自定义导入类型的扩展名（例如：.vue），因为它会干扰 IDE 和类型支持。
 		extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
 	}
+
 	return viteResolve
 }
