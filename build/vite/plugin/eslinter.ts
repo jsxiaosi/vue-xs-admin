e99abb60e31f1f366b1195437d08840c7ea99bcf
SuperCuteXiaoSi <1531733886@qq.com>
2021-12-02 17:12:13 +0800

    fixbug: 删除多余的东西

--- build/vite/plugin/eslinter.ts
+++ build/vite/plugin/eslinter.ts
@@ -7,19 +7,24 @@ import type { ConfigEnv } from 'vite'
 
 export function configEsLinterPlugin(configEnv: ConfigEnv) {
 	const esLinterPlugin = linterPlugin({
+		// 包括检查的文件
 		include: [
 			'./src/**/*.ts',
 			'./src/**/*.tsx',
 			'./src/**/*.vue',
 			'./src/**/*.js',
 		],
+		//运行的lint类型（这里运行了eslint typeScriptLint）注意：目前这个插件好像还没支持styleLint没看到有相关的define
 		linters: [
 			new EsLinter({
+				// 环境变量
 				configEnv: configEnv,
+				// 运行时是否删除缓存文件
 				serveOptions: { clearCacheOnStart: true },
 			}),
 			new TypeScriptLinter(),
 		],
+		// 检查文件的范围 processedFiles：仅对vite处理的文件检查  filesInFolder：全部文件都检查
 		build: {
 			includeMode: 'filesInFolder',
 		},
