a3f57e01157153cc95c69bb387f22fe790e3d9fc
SuperCuteXiaoSi <1531733886@qq.com>
2022-01-03 11:33:40 +0800

    style: -

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-	'*.{js,ts}': ['eslint --fix', 'prettier --write'],
-	'package.json': ['prettier --write'],
-	'*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
-	'*.scss': ['stylelint --fix', 'prettier --write'],
-	'*.md': ['prettier --write'],
-}
+  '*.{js,ts,tsx,jsx}': ['eslint --fix', 'prettier --write'],
+  'package.json': ['prettier --write'],
+  '*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
+  '*.{scss,less,styl,html}': ['stylelint --fix', 'prettier --write'],
+  '*.md': ['prettier --write'],
+};
