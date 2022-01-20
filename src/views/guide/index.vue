49c173fd926d176e8fb5d3227357a17b7f463238
SuperCuteXiaoSi <1531733886@qq.com>
2022-01-20 09:35:03 +0800

    refactor: 删除useDraggable

--- src/views/guide/index.vue
+++ src/views/guide/index.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="page-container">
     <el-button type="primary" @click="handleIntro">引导页</el-button>
-    <div ref="el" :style="style" style="position: fixed; z-index: 99999">
-      Drag me! I am at {{ x }}, {{ y }}
-    </div>
   </div>
 </template>
 
@@ -14,15 +11,6 @@
       prevLabel: '单独设置的上一步',
     });
   };
-  import { useDraggable } from '@vueuse/core';
-  import { ref } from 'vue';
-
-  const el = ref<HTMLElement | null>(null);
-
-  // `style` will be a helper computed for `left: ?px; top: ?px;`
-  const { x, y, style } = useDraggable(el, {
-    initialValue: { x: 40, y: 40 },
-  });
 </script>
 
 <style scoped lang="scss">
