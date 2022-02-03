064467b4244f229490081c5f86a0c66abd22a096
SuperCuteXiaoSi <1531733886@qq.com>
2022-02-03 22:33:06 +0800

    chore: 更新依赖

--- build/vite/plugin/style.ts
+++ build/vite/plugin/style.ts
@@ -2,30 +2,31 @@
  * 动态引入组件库样式
  * https://github.com/anncwb/vite-plugin-style-import/blob/main/README.zh_CN.md
  */
-import styleImport from 'vite-plugin-style-import';
+// import styleImport from 'vite-plugin-style-import';
 import type { Plugin } from 'vite';
 
 export function configStylePlugin(): Plugin | Plugin[] {
-  const options = {
-    // libs: [
-    //   {
-    //     libraryName: 'element-plus',
-    //     esModule: true,
-    //     ensureStyleFile: true,
-    //     resolveStyle: (name: any) => {
-    //       name = name.slice(3);
-    //       // 使用element scss样式
-    //       return `element-plus/packages/theme-chalk/src/${name}.scss`;
-    //       // 使用element css样式
-    //       // return `element-plus/lib/theme-chalk/${name}.css`;
-    //     },
-    //     // resolveComponent: (name: any) => {
-    //     // 	console.log(name)
-    //     // 	return `element-plus/lib/${name}`
-    //     // },
-    //   },
-    // ],
-  };
-  const plugin: Plugin[] = [styleImport(options)];
+  // const options = {
+  //   // libs: [
+  //   //   {
+  //   //     libraryName: 'element-plus',
+  //   //     esModule: true,
+  //   //     ensureStyleFile: true,
+  //   //     resolveStyle: (name: any) => {
+  //   //       name = name.slice(3);
+  //   //       // 使用element scss样式
+  //   //       return `element-plus/packages/theme-chalk/src/${name}.scss`;
+  //   //       // 使用element css样式
+  //   //       // return `element-plus/lib/theme-chalk/${name}.css`;
+  //   //     },
+  //   //     // resolveComponent: (name: any) => {
+  //   //     // 	console.log(name)
+  //   //     // 	return `element-plus/lib/${name}`
+  //   //     // },
+  //   //   },
+  //   // ],
+  // };
+  // const plugin: Plugin[] = [styleImport(options)];
+  const plugin: Plugin[] = [];
   return plugin;
 }
--- build/vite/plugin/svg.ts
+++ build/vite/plugin/svg.ts
@@ -2,11 +2,11 @@
  * svg
  * https://github.com/anncwb/vite-plugin-svg-icons/blob/main/README.zh_CN.md
  */
-import viteSvgIcons from 'vite-plugin-svg-icons';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import path from 'path';
 
 export function configSvgPlugin() {
-  const svgPlugin = viteSvgIcons({
+  const svgPlugin = createSvgIconsPlugin({
     // 指定需要缓存的图标文件夹
     iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
     // 压缩配置
