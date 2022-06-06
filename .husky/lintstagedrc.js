e6956c6ff800dfba4ed8e25d0370c50be1e185c5
SuperCuteXiaoSi <1531733886@qq.com>
2022-06-06 18:45:45 +0800

    fixbug: 修改问题

--- .husky/lintstagedrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '*.{js,ts,tsx,jsx}': ['eslint --fix', 'prettier --write'],
-  'package.json': ['prettier --write'],
-  '*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
-  '*.{scss,less,styl,html}': ['stylelint --fix', 'prettier --write'],
-  '*.md': ['prettier --write'],
-};
