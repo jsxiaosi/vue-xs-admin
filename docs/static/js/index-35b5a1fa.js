9eca65a31cba3230101008871128d90f096482a2
小斯（xiaosi） <1531733886@qq.com>
2023-07-24 23:31:38 +0800

    feat: ✨ build

---
+++ docs/static/js/index-35b5a1fa.js
@@ -0,0 +1,22 @@
+import { d as i, j as t, bv as p, o as m, c as d, t as a, y as n } from './index-ae766c94.js';
+const x = i({
+  __name: 'index',
+  setup(_) {
+    let o = t('admin');
+    function l() {
+      let s = t(0),
+        c = t(0);
+      return { x: s, y: c };
+    }
+    let { x: e, y: r } = p(l());
+    console.log(e);
+    const u = () => {
+      (o.value = 'update'), (e.value = 200);
+    };
+    return (s, c) => (
+      m(),
+      d('div', { class: 'page-container', onClick: u }, a(n(o)) + '-' + a(n(e)) + '-' + a(n(r)), 1)
+    );
+  },
+});
+export { x as default };
---
+++ docs/static/js/slotsHelper-5252f8f0.js
@@ -0,0 +1,8 @@
+import { cB as t } from './index-ae766c94.js';
+function f(n, r = 'default', u) {
+  if (!n || !Reflect.has(n, r)) return null;
+  if (!t(n[r])) return console.error(`${r} is not a function!`), null;
+  const e = n[r];
+  return e ? e(u) : null;
+}
+export { f as g };
