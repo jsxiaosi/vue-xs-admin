46372e7ac7b27b0849c420acb3146301e5d4808b
小斯（xiaosi） <1531733886@qq.com>
2023-11-13 23:48:02 +0800

    feat: ✨ tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
