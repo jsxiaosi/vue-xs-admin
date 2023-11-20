b829131ab84affd74e4d05101fc82d6d7bf19977
小斯（xiaosi） <1531733886@qq.com>
2023-11-20 21:41:57 +0800

    feat: ✨ vite
    - 升级vite5 beta

--- build/vite/plugin/buildOuteInfo.ts
+++ build/vite/plugin/buildOuteInfo.ts
@@ -56,8 +56,8 @@ export function viteBuildOuteInfo(): Plugin {
     // 该插件在 plugin-vue 插件之前执行，这样就可以直接解析到原模板文件
     enforce: 'post',
     transformIndexHtml: {
-      enforce: 'post',
-      transform: () => {},
+      order: 'post',
+      handler: () => {},
     },
 
     configResolved(resolvedConfig) {
